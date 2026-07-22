ebaacd546b8574ae5acb4174c78d1a868abc66ce
KaleSG <jeremiahsuarez06@gmail.com>
2026-07-22 14:56:09 -0700

    update readme

--- helper-scripts/calculators/tps54360ddar.py
+++ helper-scripts/calculators/tps54360ddar.py
@@ -21,7 +21,7 @@ UVLO_VOLTAGE = 4.3 # Volts ( Default is 4.3 volts )
 
 # Safety Margins
 V_IN_MARGIN = .1
-I_OUT_MAX_MARGIN = .15
+I_OUT_MAX_MARGIN = 1
 T_MIN_ON_MARGIN = .1
 MAX_K_IND_RATIO = .3
 
@@ -31,11 +31,14 @@ from helpers import *
 
 def computeComponentValues(v_in_nominal: float, v_in_max:float, v_out: float, i_out_max: float, catch_diode_voltage: float = .7):
     safe_t_min_on = computeMarginedValue(T_MIN_ON,T_MIN_ON_MARGIN)
-    target_switch_speed = computeOptimalSwitchingSpeed(v_in_nominal,v_out,safe_t_min_on)
+    safe_i_out_max = computeMarginedValue(i_out_max, I_OUT_MAX_MARGIN)
+
+    target_switch_frequency = computeOptimalSwitchingSpeed(v_in_nominal,v_out,safe_t_min_on)
     
     # Compute Minimum Viable Inductor Value
-
+    safe_l_out_min = computeMinInductorValue(v_in_max,v_out,safe_i_out_max,target_switch_frequency)
     # Compute Output Capacitor Value
+
 
     # Operate on Catch Diode if necessary, currently, the B560C-13-F Catch Schottcky Diode is forward voltage is set to default
 
@@ -47,16 +50,29 @@ def computeComponentValues(v_in_nominal: float, v_in_max:float, v_out: float, i_
 
     # Compute Resistor Division for Desired V_Out
     
-    return None
+    return safe_l_out_min
 
 
-# Compute optimal switching speed from desired parameters
+# Compute optimal switching speed from desired parameters (in Hz)
 # D = v_out / v_in = t_min_on / t_period
 # F = v_out / ( v_in * t_min_on )
-# note: t_min_on is in nanoseconds
+# Units:
+#   v_in: Volts
+#   v_out: Volts
+#   t_min_on: nanoseconds
+#   
+#
 def computeOptimalSwitchingSpeed(v_in, v_out, t_min_on):
     t_min_on_seconds = t_min_on * pow(10,-9)
     return round(v_out / ( v_in * t_min_on_seconds))
 
-def computeMinInductorValue(v_in_max,v_out, i_out_max, switch_frequency):
-    return
+# Compute Minimum Inductor Value
+# Units: 
+#   v_in_max: Volts
+#   v_out: Volts
+#   i_out_max: Amperes
+#   switch_frequency: Hz
+#
+#
+def computeMinInductorValue(v_in_max: float,v_out: float, i_out_max: float, switch_frequency: float):
+    return ((v_in_max - v_out) / (i_out_max * MAX_K_IND_RATIO)) * v_out / (v_in_max * switch_frequency)
